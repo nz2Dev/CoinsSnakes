12d809fcd88bc65cb21b267368d7d5cd798e9938
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-08-07 19:03:38 +0300

    Implement basic random spawn position within world bounds

--- app/src/main/java/com/pinonzhyk/coinssnake/game/SnakeSpawnSystem.java
+++ app/src/main/java/com/pinonzhyk/coinssnake/game/SnakeSpawnSystem.java
@@ -6,7 +6,7 @@ import com.pinonzhyk.coinssnake.world.WorldObject;
 public class SnakeSpawnSystem extends World.System implements World.UpdateSystem {
 
     private float lastSpawnTime;
-    private float spawnInterval = 3f;
+    private float spawnInterval = 5f;
 
     @Override
     public void onUpdate(float timeSec, float deltaTime) {
@@ -26,7 +26,7 @@ public class SnakeSpawnSystem extends World.System implements World.UpdateSystem
         }
 
         lastSpawnTime = timeSec;
-        float startCoord = world().getBoundsWidthUnits() * 0.1f;
+        float startCoord = (float) (world().getBoundsWidthUnits() * (Math.random() * 0.8 + 0.1));
         WorldObject snake = new WorldObject(startCoord, startCoord);
         snake.addComponent(new Snake());
         world().instantiateWorldObject(snake);
