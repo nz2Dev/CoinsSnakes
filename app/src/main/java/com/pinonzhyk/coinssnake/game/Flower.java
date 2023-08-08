e7cc3574a0822e6cb17aad54627c6ad2df64f742
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-08-08 18:02:12 +0300

    Implement flower coins yielding

--- app/src/main/java/com/pinonzhyk/coinssnake/game/Flower.java
+++ app/src/main/java/com/pinonzhyk/coinssnake/game/Flower.java
@@ -1,7 +1,29 @@
 package com.pinonzhyk.coinssnake.game;
 
+import com.pinonzhyk.coinssnake.world.World;
 import com.pinonzhyk.coinssnake.world.WorldObject;
 
-public class Flower extends WorldObject.Component {
+public class Flower extends WorldObject.Component implements WorldObject.UpdateReceiver {
 
+    private float lastYieldCheckTime;
+    private float yieldInterval;
+    private int earnPerYield;
+
+    public Flower() {
+        lastYieldCheckTime = -1;
+        yieldInterval = 3f;
+        earnPerYield = 1;
+    }
+
+    @Override
+    public void onUpdate(float timeSec) {
+        if (lastYieldCheckTime < 0) {
+            lastYieldCheckTime = timeSec;
+        }
+
+        if (lastYieldCheckTime + yieldInterval < timeSec) {
+            lastYieldCheckTime = timeSec;
+            world().findSystem(EconomySystem.class).earn(earnPerYield);
+        }
+    }
 }
