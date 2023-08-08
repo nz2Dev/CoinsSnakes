87f529c8adbb378eeae246fa52c2c8d6b7d2f66a
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-08-08 18:35:22 +0300

    Implement snake spawn interval degradation

--- app/src/main/java/com/pinonzhyk/coinssnake/game/SnakeSpawnSystem.java
+++ app/src/main/java/com/pinonzhyk/coinssnake/game/SnakeSpawnSystem.java
@@ -7,9 +7,19 @@ public class SnakeSpawnSystem extends World.System implements World.UpdateSystem
 
     private float lastSpawnTime;
     private float spawnInterval = 5f;
+    private float lastSpawningFrequencyCheckTime = -1;
+    private float spawningFrequencyCheckInterval = 10;
 
     @Override
     public void onUpdate(float timeSec, float deltaTime) {
+        if (lastSpawningFrequencyCheckTime < 0) {
+            lastSpawningFrequencyCheckTime = timeSec;
+        }
+        if (lastSpawningFrequencyCheckTime + spawningFrequencyCheckInterval < timeSec) {
+            lastSpawningFrequencyCheckTime = timeSec;
+            spawnInterval -= spawnInterval * 0.05f;
+        }
+
         final GameManager gameManager = world().findSystem(GameManager.class);
         if (!gameManager.isSnakeSpawnAllowed() || lastSpawnTime + spawnInterval > timeSec) {
             return;
